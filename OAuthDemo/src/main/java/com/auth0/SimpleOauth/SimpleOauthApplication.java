708089c17763187030a734527c42e28c3852a711
Nisanth Selvaraj <121391945+Nisanth2004@users.noreply.github.com>
2024-01-18 18:57:28 +0530

    initial commit

---
+++ OAuthDemo/src/main/java/com/auth0/SimpleOauth/SimpleOauthApplication.java
@@ -0,0 +1,13 @@
+package com.auth0.SimpleOauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOauthApplication.class, args);
+	}
+
+}
